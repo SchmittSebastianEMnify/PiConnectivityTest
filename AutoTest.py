405d8fa61e31c2a9f8d4692c68321f0b03c49541
Timon33 <timon.giess@gmx.de>
2019-08-28 08:47:08 +0200

    Commit 2

--- AutoTest.py
+++ AutoTest.py
@@ -1,6 +1,30 @@
+import sh
+import subprocess
+import dns
+import dns.resolver
+import socket
+import urllib.request
 
+address = "8.8.8.8"
+print("-----------")
 
-#Test to push to GitHub Account
+pingOutput = str(subprocess.check_output(['ping', '-c', '1', '-s', '1024', address]))
+lines = pingOutput.split("\\n")
+for line in lines:
+    print(line)
 
-a = 5+2
-print(a)
+print("-----------")
+
+myResolver = dns.resolver.Resolver()
+myAnswers = myResolver.query("www.reddit.com", "A")
+for rdata in myAnswers:
+    print(rdata)
+
+print("-----------")
+
+print(socket.gethostbyname('www.youtube.com'))
+
+print("-----------")
+
+url = 'http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg'
+urllib.request.urlretrieve(url, '/home/user/Downloads/cat.jpg')
